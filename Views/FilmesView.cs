5a87c4644d68a5b3f4427554102b173225fcb1f2
Cybermacks <nnexxus15@proton.me>
2025-02-04 13:30:32 -0300

    FilmesView.cs

--- Views/FilmesView.cs
+++ Views/FilmesView.cs
@@ -444,50 +444,6 @@ namespace Aniflix
                 editando = false;
             }
         }
-        private static void DoReadOnly(Control control)
-        {
-            foreach (Control c in control.Controls)
-            {
-                if (c.Controls != null && c.Controls.Count > 0)
-                {
-                    DoReadOnly(c);
-                }
-                else if (c is TextBox box)
-                {
-                    box.ReadOnly = true;
-                }
-                else if (c is ComboBox comboBox)
-                {
-                    comboBox.Enabled = false;
-                }
-                else if (c is MaskedTextBox maskTextBox)
-                {
-                    maskTextBox.ReadOnly = true;
-                }
-            }
-        }
 
-        private static void UndoReadOnly(Control control)
-        {
-            foreach (Control c in control.Controls)
-            {
-                if (c.Controls != null && c.Controls.Count > 0)
-                {
-                    UndoReadOnly(c);
-                }
-                else if (c is TextBox box)
-                {
-                    box.ReadOnly = false;
-                }
-                else if (c is ComboBox comboBox)
-                {
-                    comboBox.Enabled = true;
-                }
-                else if (c is MaskedTextBox maskTextBox)
-                {
-                    maskTextBox.ReadOnly = false;
-                }
-            }
-        }
     }
 }
