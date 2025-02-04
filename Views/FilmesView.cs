787930dac981c97fe4c41caa2d3af4dc2ccd9f77
Cybermacks <nnexxus15@proton.me>
2025-02-04 20:44:52 -0300

    FilmesView.Designer.cs

--- Views/FilmesView.cs
+++ Views/FilmesView.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Aniflix.Services;
+using Aniflix.Extensions;
+using System.Globalization;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aniflix.Views
 {
@@ -16,5 +19,190 @@ namespace Aniflix.Views
         {
             InitializeComponent();
         }
+
+        private async Task GetFilmesAsync()
+        {
+            try
+            {
+
+                var client = new TMDbLib.Client.TMDbClient("1dcbf681735d3e7454953f5b7c22b6dc")
+                {
+                    DefaultLanguage = "pt-BR",
+                    DefaultCountry = "BR",
+                };
+
+                var movieTask = client.GetMovieAsync(CodigoText.Text);
+                var creditsTask = client.GetMovieCreditsAsync(Convert.ToInt32(CodigoText.Text));
+
+                await Task.WhenAll(movieTask, creditsTask);
+
+                var movie = movieTask.Result;
+                var credits = creditsTask.Result;
+
+                if (movie != null)
+                {
+                    Invoke((Action)(() =>
+                    {
+                        TituloText.Text = movie.Title;
+                        SinopseText.Text = movie.Overview;
+                        TituloOriginalText.Text = movie.OriginalTitle;
+                        DataLancamentoText.Text = movie.ReleaseDate?.ToString("dd/MM/yyyy");
+                    }));
+
+                    if (
+                        DateTime.TryParseExact(
+                            DataLancamentoText.Text,
+                            "dd/MM/yyyy",
+                            CultureInfo.InvariantCulture,
+                            DateTimeStyles.None,
+                            out var releaseData
+                        )
+                    )
+                    {
+                        string ano = releaseData.Year.ToString();
+                        TagsText.Text = $"#Filme #Filme{ano}";
+                    }
+
+                    if (movie.Genres != null && movie.Genres.Count > 2)
+                    {
+                        var hashtags = new HashSet<string>();
+
+                        static void FormatGenre(string genre, HashSet<string> hashtags)
+                        {
+                            Dictionary<string, string> specialWords = new()
+                    {
+
+                        { "ficção científica", "ficcaocientifica ficçãocientífica" },
+                        { "romântico", "romantico romântico" },
+                        { "romântica", "romantica romântica" },
+                        { "comédia", "comedia comédia" },
+                        { "mistério", "misterio mistério" },
+                        { "ação", "acao ação" }
+                    };
+
+                            string lowerGenre = genre.ToLower();
+
+                            if (specialWords.TryGetValue(lowerGenre, out string? value))
+                            {
+                                foreach (var tag in value.Split(' '))
+                                {
+                                    hashtags.Add($"#{tag}");
+                                }
+                            }
+                            else
+                            {
+                                string clean = new(genre.RemoveDiacritics().Where(char.IsAscii).ToArray());
+                                hashtags.Add($"#{genre.ToLower().Replace(" ", "")}");
+                                hashtags.Add($"#{clean.ToLower().Replace(" ", "")}");
+                            }
+                        }
+
+                        FormatGenre(movie.Genres[0].Name, hashtags);
+                        FormatGenre(movie.Genres[1].Name, hashtags);
+                        FormatGenre(movie.Genres[2].Name, hashtags);
+
+                        GeneroText.Text = string.Join(" ", hashtags);
+                    }
+                }
+                else
+                {
+                    Invoke(() =>
+                    {
+                        MessageBox.Show("Nenhum filme encontrado.", "Filmes - Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        CodigoText.Focus();
+                    });
+                }
+
+                if (credits != null && credits.Crew != null)
+                {
+                    var directors = credits
+                    .Crew.Where(person => person.Job == "Director")
+                    .Take(4)
+                    .Select(person => $"#{person.Name.Replace(" ", "")}")
+                    .ToList();
+
+                    Invoke((Action)(() =>
+                    {
+                        DiretorText.Text = string.Join(" ", StringExtensions.ClearLists(directors));
+                    }));
+                }
+                else
+                {
+                    Invoke((Action)(() =>
+                    {
+                        DiretorText.Text = string.Empty;
+                    }));
+                }
+
+                if (credits != null && credits.Cast != null)
+                {
+
+                    var stars = credits
+                    .Cast.Take(5)
+                    .Select(person => $"#{person.Name.Replace(" ", "")}")
+                    .ToList();
+                    Invoke((Action)(() =>
+                    {
+                        EstrelasText.Text = string.Join(" ", StringExtensions.ClearLists(stars));
+                    }));
+                }
+                else
+                {
+                    Invoke((Action)(() =>
+                    {
+                        EstrelasText.Text = string.Empty;
+                    }));
+                }
+
+                if (movie != null && movie.ProductionCompanies != null)
+                {
+
+                    var studios = movie!
+                    .ProductionCompanies.Take(5)
+                    .Select(company => $"#{company.Name.Replace(" ", "")}")
+                    .ToList();
+                    Invoke((Action)(() =>
+                    {
+                        EstudioText.Text = string.Join(" ", StringExtensions.ClearLists(studios));
+                    }));
+                }
+                else
+                {
+                    Invoke((Action)(() =>
+                    {
+                        EstudioText.Text = string.Empty;
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke((Action)(() =>
+                {
+                    MessageBox.Show($"Erro ao buscar o filme: {ex.Message}", "Filmes - Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }));
+            }
+        }
+
+        private void UpdateData()
+        {
+            var model = new FilmesServices(
+                titulo: TituloText.Text,
+                audio: AudioBox.SelectedItem?.ToString() ?? string.Empty,
+                sinopse: SinopseText.Text,
+                tituloOriginal: TituloOriginalText.Text,
+                dataLancamento: DataLancamentoText.Text,
+                tituloAlternativo: TituloAlternativoText.Text,
+                franquia: FranquiaText.Text,
+                genero: GeneroText.Text,
+                tags: TagsText.Text,
+                diretor: DiretorText.Text,
+                  mcu: FaseMCUText.Text,
+                estrelas: EstrelasText.Text,
+                estudio: EstudioText.Text
+
+                );
+            ResumoText.Text = model.GetFormattedText();
+        }
+
     }
 }
