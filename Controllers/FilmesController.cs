e64d8573046ad194d50c99a98960129a89c8deed
Cybermacks <nnexxus15@proton.me>
2025-02-03 23:38:18 -0300

    FilmesController.cs

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -11,6 +11,7 @@ namespace Aniflix.Controllers
         public static void InsereNovoFilme(Filmes filmes)
         {
             var filme = _conn.ExecuteQuery("SELECT * FROM filmes WHERE codigo = @Codigo", new MySqlParameter("@Codigo", filmes.Codigo));
+
             if (filme != null)
             {
                 MessageBox.Show("Filme " + filmes.Titulo + " já está cadastrado!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -18,24 +19,22 @@ namespace Aniflix.Controllers
             }
             else
             {
-                int rowsAffected = _conn.Execute("INSERT INTO filmes (codigo, titulo, audio, sinopse, titulo_original, data_lancamento, titulo_alternativo, franquia, genero, tags, diretor, mcu, estrelas, estudio) VALUES (@Codigo, @Titulo, @Audio, @Sinopse, @TituloOriginal, @DataLancamento, @TituloAlternativo, @Franquia, @Genero, @Tags, @Diretor, @MCU, @Estrelas, @Estudio)",
-                    new
-                    {
-                        filmes.Codigo,
-                        filmes.Titulo,
-                        filmes.Audio,
-                        filmes.Sinopse,
-                        filmes.TituloOriginal,
-                        filmes.DataLancamento,
-                        filmes.TituloAlternativo,
-                        filmes.Franquia,
-                        filmes.Genero,
-                        filmes.Tags,
-                        filmes.Diretor,
-                        filmes.MCU,
-                        filmes.Estrelas,
-                        filmes.Estudio
-                    });
+                int rowsAffected = _conn.InsertData("INSERT INTO filmes (codigo, titulo, audio, sinopse, titulo_original, data_lancamento, titulo_alternativo, franquia, genero, tags, diretor, mcu, estrelas, estudio) VALUES (@Codigo, @Titulo, @Audio, @Sinopse, @TituloOriginal, @DataLancamento, @TituloAlternativo, @Franquia, @Genero, @Tags, @Diretor, @MCU, @Estrelas, @Estudio)",
+                    new MySqlParameter("@Codigo", filmes.Codigo),
+                    new MySqlParameter("@Titulo", filmes.Titulo),
+                    new MySqlParameter("@Audio", filmes.Audio),
+                    new MySqlParameter("@Sinopse", filmes.Sinopse),
+                    new MySqlParameter("@TituloOriginal", filmes.TituloOriginal),
+                    new MySqlParameter("@DataLancamento", filmes.DataLancamento),
+                    new MySqlParameter("@TituloAlternativo", filmes.TituloAlternativo),
+                    new MySqlParameter("@Franquia", filmes.Franquia),
+                    new MySqlParameter("@Genero", filmes.Genero),
+                    new MySqlParameter("@Tags", filmes.Tags),
+                    new MySqlParameter("@Diretor", filmes.Diretor),
+                    new MySqlParameter("@MCU", filmes.MCU),
+                    new MySqlParameter("@Estrelas", filmes.Estrelas),
+                    new MySqlParameter("@Estudio", filmes.Estudio)
+                        );
 
                 if (rowsAffected > 0)
                 {
