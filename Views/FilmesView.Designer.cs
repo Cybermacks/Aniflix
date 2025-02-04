1d3813a87c277d5a26d1f2dcec37ecd245c1cbb1
Cybermacks <nnexxus15@proton.me>
2025-02-04 20:40:20 -0300

    FilmesView.Designer.cs FilmesView.cs FilmesView.resx

---
+++ Views/FilmesView.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Aniflix.Views
+{
+    partial class FilmesView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FilmesView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1468, 788);
+            Name = "FilmesView";
+            Text = "FilmesView";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
