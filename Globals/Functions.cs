c9cf129e744e3d321ac1aad98d33d8932fd7ceaf
Cybermacks <nnexxus15@proton.me>
2025-02-04 13:31:30 -0300

    Functions.cs

--- Globals/Functions.cs
+++ Globals/Functions.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aniflix.Globals
+﻿namespace Aniflix.Globals
 {
-    internal class Functions
+    public static class Functions
     {
+        public static void DoReadOnly(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c.Controls != null && c.Controls.Count > 0)
+                {
+                    DoReadOnly(c);
+                }
+                else if (c is TextBox box)
+                {
+                    box.ReadOnly = true;
+                }
+                else if (c is ComboBox comboBox)
+                {
+                    comboBox.Enabled = false;
+                }
+                else if (c is MaskedTextBox maskTextBox)
+                {
+                    maskTextBox.ReadOnly = true;
+                }
+            }
+        }
+
+        public static void UndoReadOnly(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c.Controls != null && c.Controls.Count > 0)
+                {
+                    UndoReadOnly(c);
+                }
+                else if (c is TextBox box)
+                {
+                    box.ReadOnly = false;
+                }
+                else if (c is ComboBox comboBox)
+                {
+                    comboBox.Enabled = true;
+                }
+                else if (c is MaskedTextBox maskTextBox)
+                {
+                    maskTextBox.ReadOnly = false;
+                }
+            }
+        }
     }
 }
