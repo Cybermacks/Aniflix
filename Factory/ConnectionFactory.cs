61de725761cae93f309b6348f84a582569c28e21
Cybermacks <nnexxus15@proton.me>
2025-02-03 23:22:33 -0300

    Update ConnectionFactory.cs

--- Factory/ConnectionFactory.cs
+++ Factory/ConnectionFactory.cs
@@ -3,27 +3,60 @@ using MySqlConnector;
 
 namespace Aniflix.Factory
 {
-    public class ConnectionFactory
+    public class DatabaseConnection
     {
-        private static ConnectionFactory? _instance;
-        private readonly MySqlConnection _connection;
-        private const string ConnectionString = "Server=localhost;Database=aniflix;Username=Covenant9687;Password=v*##GLBkB3r9tuUt;";
+        private static readonly Lazy<DatabaseConnection> instance =
+           new(() => new DatabaseConnection());
 
-        private ConnectionFactory()
+        private readonly MySqlConnection connection;
+
+        private const string connectionString = "Server=localhost;Database=aniflix;Username=Covenant9687;Password=v*##GLBkB3r9tuUt;";
+
+        private DatabaseConnection()
         {
-            _connection = new MySqlConnection(ConnectionString);
-            _connection.Open();
+            connection = new MySqlConnection(connectionString);
         }
 
-        public static ConnectionFactory GetInstance()
+        public static DatabaseConnection Instance => instance.Value;
+
+        public MySqlConnection GetConnection()
         {
-            _instance ??= new ConnectionFactory();
-            return _instance;
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            return connection;
         }
 
-        public IDbConnection GetConnection()
+        public void CloseConnection()
         {
-            return _connection;
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public int ExecuteQuery(string query, params MySqlParameter[] parameters)
+        {
+            using var cmd = new MySqlCommand(query, GetConnection());
+            if (parameters != null)
+                cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public DataTable GetDataTable(string query, params MySqlParameter[] parameters)
+        {
+            using var cmd = new MySqlCommand(query, GetConnection());
+            if (parameters != null)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+
+            using var adapter = new MySqlDataAdapter(cmd);
+            var table = new DataTable();
+            adapter.Fill(table);
+            return table;
         }
     }
 }
+
