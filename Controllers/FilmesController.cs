c66fc83e2d3bb1862b0f8287b9d7408a6831d888
Cybermacks <nnexxus15@proton.me>
2025-02-03 23:42:28 -0300

    FilmesController.cs

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -42,7 +42,7 @@ namespace Aniflix.Controllers
                 }
             }
         }
-        public static Filmes? GetFirstFilme()
+        public static string GetFirstFilme()
         {
             return _conn.GetFirstRecord("filmes");
         }
