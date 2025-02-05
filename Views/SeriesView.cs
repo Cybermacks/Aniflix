9e8a0772b12465a7f9dccf1d42456550be55b03f
Cybermacks <nnexxus15@proton.me>
2025-02-05 19:57:28 -0300

    FilmesView.cs

--- Views/SeriesView.cs
+++ Views/SeriesView.cs
@@ -2,6 +2,8 @@ namespace Aniflix;
 
 public partial class SeriesView : Form
 {
+    private int currentId = 0;
+    private bool editando = false;
     public SeriesView()
     {
         InitializeComponent();
