b08dabca027f5ecb241257ef95f46fbc402c1090
Cybermacks <nnexxus15@proton.me>
2025-02-06 17:38:19 -0300

    GlobalVars.cs

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -5,8 +5,8 @@ namespace Aniflix.Globals
     public static class GlobalVars
     {
         public static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
-        public static readonly int currentId = 0;
-        public static readonly bool editando = false;
-        public static readonly string API_KEY = "1dcbf681735d3e7454953f5b7c22b6dc";
+        public static int currentId = 0;
+        public static bool editando = false;
+        public static string API_KEY = "1dcbf681735d3e7454953f5b7c22b6dc";
     }
 }
