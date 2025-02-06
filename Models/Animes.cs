a31a4aa86059371b8c8b6b0ef6d94efe835d8f9e
Cybermacks <nnexxus15@proton.me>
2025-02-06 14:07:04 -0300

    Animes.cs

--- Models/Animes.cs
+++ Models/Animes.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aniflix.Models
+﻿namespace Aniflix.Models
 {
-    internal class Animes
+    public class Animes
     {
+        public string? Codigo { get; set; }
+        public string? Titulo { get; set; }
+        public string? Audio { get; set; }
+        public string? Sinopse { get; set; }
+        public string? TituloOriginal { get; set; }
+        public string? DataLancamento { get; set; }
+        public string? TituloAlternativo { get; set; }
+        public string? PaisOrigem { get; set; }
+        public string? IdiomaOriginal { get; set; }
+        public string? Serie { get; set; }
+        public string? Autores { get; set; }
+        public string? Criadores { get; set; }
+        public string? ObraOriginal { get; set; }
+        public string? Genero { get; set; }
+        public string? Tags { get; set; }
+        public string? Diretor { get; set; }
+        public string? MCU { get; set; }
+        public string? Estrelas { get; set; }
+        public string? Estudio { get; set; }
     }
 }
