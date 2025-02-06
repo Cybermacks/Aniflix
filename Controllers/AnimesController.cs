ac351dad6a0ccb8b82c106403efcd0f189b85a63
Cybermacks <nnexxus15@proton.me>
2025-02-06 14:08:51 -0300

    AnimesController.cs

--- Controllers/AnimesController.cs
+++ Controllers/AnimesController.cs
@@ -14,77 +14,77 @@ namespace Aniflix.Controllers
     {
         public static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
 
-        public static void Registrar(Series series)
+        public static void Registrar(Animes animes)
         {
-            var serie = _conn.SelectData("series", "codigo = @codigo", new MySqlParameter("@codigo", series.Codigo));
+            var serie = _conn.SelectData("animes", "codigo = @codigo", new MySqlParameter("@codigo", animes.Codigo));
 
             if (serie.Rows.Count > 0)
             {
-                MessageBox.Show(series.Titulo + " já está cadastrada!", "Séries", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(animes.Titulo + " já está cadastrada!", "Séries", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             else
             {
-                int rowsAffected = _conn.InsertData("series",
-                    new MySqlParameter("@codigo", series.Codigo),
-                    new MySqlParameter("@titulo", series.Titulo),
-                    new MySqlParameter("@audio", series.Audio),
-                    new MySqlParameter("@sinopse", series.Sinopse),
-                    new MySqlParameter("@titulo_original", series.TituloOriginal),
-                    new MySqlParameter("@data_lancamento", series.DataLancamento),
-                    new MySqlParameter("@titulo_alternativo", series.TituloAlternativo),
-                    new MySqlParameter("@pais_origem", series.PaisOrigem),
-                    new MySqlParameter("@idioma_original", series.IdiomaOriginal),
-                    new MySqlParameter("@anime", series.Serie),
-                    new MySqlParameter("@autores", series.Autores),
-                    new MySqlParameter("@genero", series.Genero),
-                    new MySqlParameter("@tags", series.Tags),
-                    new MySqlParameter("@diretor", series.Diretor),
-                    new MySqlParameter("@estrelas", series.Estrelas),
-                    new MySqlParameter("@estudio", series.Estudio)
+                int rowsAffected = _conn.InsertData("animes",
+                    new MySqlParameter("@codigo", animes.Codigo),
+                    new MySqlParameter("@titulo", animes.Titulo),
+                    new MySqlParameter("@audio", animes.Audio),
+                    new MySqlParameter("@sinopse", animes.Sinopse),
+                    new MySqlParameter("@titulo_original", animes.TituloOriginal),
+                    new MySqlParameter("@data_lancamento", animes.DataLancamento),
+                    new MySqlParameter("@titulo_alternativo", animes.TituloAlternativo),
+                    new MySqlParameter("@pais_origem", animes.PaisOrigem),
+                    new MySqlParameter("@idioma_original", animes.IdiomaOriginal),
+                    new MySqlParameter("@anime", animes.Serie),
+                    new MySqlParameter("@autores", animes.Autores),
+                    new MySqlParameter("@genero", animes.Genero),
+                    new MySqlParameter("@tags", animes.Tags),
+                    new MySqlParameter("@diretor", animes.Diretor),
+                    new MySqlParameter("@estrelas", animes.Estrelas),
+                    new MySqlParameter("@estudio", animes.Estudio)
                 );
 
                 if (rowsAffected > 0)
                 {
-                    MessageBox.Show(series.Titulo + " cadastrada com sucesso!", "Séries", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(animes.Titulo + " cadastrada com sucesso!", "Séries", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
         public static DataRow? MostraPrimeiro()
         {
-            return _conn.GetFirstRecord("series");
+            return _conn.GetFirstRecord("animes");
         }
         public static DataRow? MoveAnterior(int id)
         {
-            return _conn.GetPreviousRecord("series", id);
+            return _conn.GetPreviousRecord("animes", id);
         }
         public static DataRow? MoveProximo(int id)
         {
-            return _conn.GetNextRecord("series", id);
+            return _conn.GetNextRecord("animes", id);
         }
 
-        public static void AtualizaDados(Series series)
+        public static void AtualizaDados(animes animes)
         {
-            int rowsAffected = _conn.UpdateData("series", "codigo = @codigo",
-                new MySqlParameter("@codigo", series.Codigo),
-                new MySqlParameter("@titulo", series.Titulo),
-                new MySqlParameter("@audio", series.Audio),
-                new MySqlParameter("@sinopse", series.Sinopse),
-                new MySqlParameter("@titulo_original", series.TituloOriginal),
-                new MySqlParameter("@data_lancamento", series.DataLancamento),
-                new MySqlParameter("@titulo_alternativo", series.TituloAlternativo),
-                new MySqlParameter("@pais_origem", series.PaisOrigem),
-                new MySqlParameter("@idioma_original", series.IdiomaOriginal),
-                new MySqlParameter("@serie", series.Serie),
-                new MySqlParameter("@autores", series.Autores),
-                new MySqlParameter("@criadores", series.Criadores),
-                new MySqlParameter("@obra_original", series.PaisOrigem),
-                new MySqlParameter("@genero", series.Genero),
-                new MySqlParameter("@tags", series.Tags),
-                new MySqlParameter("@diretor", series.Diretor),
-                new MySqlParameter("@mcu", series.MCU),
-                new MySqlParameter("@estrelas", series.Estrelas),
-                new MySqlParameter("@estudio", series.Estudio)
+            int rowsAffected = _conn.UpdateData("animes", "codigo = @codigo",
+                new MySqlParameter("@codigo", animes.Codigo),
+                new MySqlParameter("@titulo", animes.Titulo),
+                new MySqlParameter("@audio", animes.Audio),
+                new MySqlParameter("@sinopse", animes.Sinopse),
+                new MySqlParameter("@titulo_original", animes.TituloOriginal),
+                new MySqlParameter("@data_lancamento", animes.DataLancamento),
+                new MySqlParameter("@titulo_alternativo", animes.TituloAlternativo),
+                new MySqlParameter("@pais_origem", animes.PaisOrigem),
+                new MySqlParameter("@idioma_original", animes.IdiomaOriginal),
+                new MySqlParameter("@serie", animes.Serie),
+                new MySqlParameter("@autores", animes.Autores),
+                new MySqlParameter("@criadores", animes.Criadores),
+                new MySqlParameter("@obra_original", animes.PaisOrigem),
+                new MySqlParameter("@genero", animes.Genero),
+                new MySqlParameter("@tags", animes.Tags),
+                new MySqlParameter("@diretor", animes.Diretor),
+                new MySqlParameter("@mcu", animes.MCU),
+                new MySqlParameter("@estrelas", animes.Estrelas),
+                new MySqlParameter("@estudio", animes.Estudio)
             );
 
             if (rowsAffected > 0)
