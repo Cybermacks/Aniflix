5e32e12276b0dbe6ec70000200c06c6a6d709d06
Cybermacks <nnexxus15@proton.me>
2025-02-05 23:20:32 -0300

    SeriesView.Designer.cs SeriesView.cs

--- Services/SeriesServices.cs
+++ Services/SeriesServices.cs
@@ -1,9 +1,9 @@
 ﻿namespace Aniflix.Services
 {
     public class SeriesServices(string titulo, string? audio, string? sinopse = null, string? tituloOriginal = null,
-               string? dataLancamento = null, string? tituloAlternativo = null, string? paisOrigem, string? idiomaOriginal, string? serie,
-               string? autores, string? criadores, string? obraOriginal,
-               string? genero = null, string? tags = null, string? diretor = null, string? mcu = null, string? estrelas = null, string? estudio = null)
+               string? dataLancamento = null, string? tituloAlternativo = null, string? paisOrigem = null, string? idiomaOriginal = null, string? serie = null,
+               string? autores = null, string? criadores = null, string? obraOriginal = null, string? genero = null, string? tags = null, string? diretor = null,
+               string? mcu = null, string? estrelas = null, string? estudio = null)
     {
         public string Titulo { get; private set; } = titulo;
         public string? Audio { get; private set; } = audio;
