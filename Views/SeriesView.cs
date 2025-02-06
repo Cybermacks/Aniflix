adae68efb1783c306825fa6388c3412c71c8111a
Cybermacks <nnexxus15@proton.me>
2025-02-05 21:31:37 -0300

    SeriesView.Designer.cs SeriesView.cs

--- Views/SeriesView.cs
+++ Views/SeriesView.cs
@@ -39,8 +39,8 @@ public partial class SeriesView : Form
                     SinopseText.Text = given.Overview;
                     TituloOriginalText.Text = given.OriginalName;
                     DataLancamentoText.Text = given.FirstAirDate?.ToString("dd/MM/yyyy");
-                    PaisOrigemText.Text = given.OriginCountry.ToString();
-                    IdiomaOriginalText.Text = given.OriginalLanguage.ToString();
+                    PaisOrigemText.Text = given.ProductionCountries[0].Name;
+                    IdiomaOriginalText.Text = given.SpokenLanguages[0].Name;
                 }));
 
                 if (
