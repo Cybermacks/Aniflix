87d458a19a01795b6a26bcf710520b78e35f3e0e
Cybermacks <nnexxus15@proton.me>
2025-02-05 22:43:06 -0300

    SeriesServices.cs

--- Services/SeriesServices.cs
+++ Services/SeriesServices.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMDbLib.Objects.General;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.Devices;
 
 namespace Aniflix.Services
 {
-    internal class SeriesServices
+    public class SeriesServices(string titulo, string franquia, string? sinopse = null, string? tituloOriginal = null,
+               string? dataLancamento = null, string? tituloAlternativo = null, string? genero = null, string? tags = null,
+               string? diretor = null, string? mcu = null, string? estrelas = null, string? estudio = null, string? audio = null)
     {
+        public string Titulo { get; private set; } = titulo;
+        public string? Audio { get; private set; } = audio;
+        public string? Sinopse { get; private set; } = sinopse;
+        public string? TituloOriginal { get; private set; } = tituloOriginal;
+        public string? DataLancamento { get; private set; } = dataLancamento;
+        public string? TituloAlternativo { get; private set; } = tituloAlternativo;
+        public string? Franquia { get; private set; } = franquia;
+        public string? Genero { get; private set; } = genero;
+        public string? Tags { get; private set; } = tags;
+        public string? Diretor { get; private set; } = diretor;
+        public string? MCU { get; private set; } = mcu;
+        public string? Estrelas { get; private set; } = estrelas;
+        public string? Estudio { get; private set; } = estudio;
+
+
+        public string GetFormattedText()
+        {
+            string formattedText =
+    $@"**{Titulo}** - **{Audio}**
+
+**HD** - __720p__
+**SD** - __480p__
+__[Os vídeos estão em ordem crescente, ou seja, de cima para baixo, tal como na descrição das resoluções.]__
+
+**Sinopse:** __{Sinopse}__
+
+**Título Original:** __{TituloOriginal}__
+**Título Alternativo:** {TituloAlternativo}
+**Data de lançamento:** __{DataLancamento}__
+**Filme:** #{Titulo.Replace(" ", string.Empty)}
+**Franquia:** {Franquia}
+**Gênero:** {Genero}
+**Tags:** {Tags}
+**Diretor:** {Diretor}
+**FaseMCU:** {MCU}
+**Estrelas:** {Estrelas}
+**Estúdio:** {Estudio?.TrimEnd()}
+";
+            return formattedText;
+        }
     }
 }
