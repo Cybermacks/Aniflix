a562fe166957fe18bae31ae20252fc88539cdcea
Cybermacks <nnexxus15@proton.me>
2025-02-06 17:29:03 -0300

    Vars.cs

--- Globals/Vars.cs
+++ Globals/Vars.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aniflix.Globals
+﻿namespace Aniflix.Globals
 {
-    internal class Vars
+    public static class Vars
     {
+
     }
 }
