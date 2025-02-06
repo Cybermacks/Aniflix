99df15eac14109ad3b65a86adb59a9c19176d5fc
Cybermacks <nnexxus15@proton.me>
2025-02-06 17:30:34 -0300

    FilmesView.cs

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -2,9 +2,12 @@
 
 namespace Aniflix.Globals
 {
-    public static class Vars
+    public static class GlobalVars
     {
         public static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
+
+        private int currentId = 0;
+        private bool editando = false;
 
 
     }
