7451973d2cc004cff111bffd60c3be8a55723dc6
Cybermacks <nnexxus15@proton.me>
2025-02-06 08:55:29 -0300

    SeriesController.cs

--- Controllers/SeriesController.cs
+++ Controllers/SeriesController.cs
@@ -9,37 +9,41 @@ namespace Aniflix.Controllers
     {
         public static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
 
-        public static void InsereNovoFilme(Filmes filmes)
+        public static void InsereNovoFilme(Series series)
         {
-            var filme = _conn.SelectData("filmes", "codigo = @codigo", new MySqlParameter("@codigo", filmes.Codigo));
+            var filme = _conn.SelectData("filmes", "codigo = @codigo", new MySqlParameter("@codigo", series.Codigo));
 
             if (filme.Rows.Count > 0)
             {
-                MessageBox.Show(filmes.Titulo + " já está cadastrado!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(series.Titulo + " já está cadastrado!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             else
             {
-                int rowsAffected = _conn.InsertData("filmes",
-                    new MySqlParameter("@codigo", filmes.Codigo),
-                    new MySqlParameter("@titulo", filmes.Titulo),
-                    new MySqlParameter("@audio", filmes.Audio),
-                    new MySqlParameter("@sinopse", filmes.Sinopse),
-                    new MySqlParameter("@titulo_original", filmes.TituloOriginal),
-                    new MySqlParameter("@data_lancamento", filmes.DataLancamento),
-                    new MySqlParameter("@titulo_alternativo", filmes.TituloAlternativo),
-                    new MySqlParameter("@franquia", filmes.Franquia),
-                    new MySqlParameter("@genero", filmes.Genero),
-                    new MySqlParameter("@tags", filmes.Tags),
-                    new MySqlParameter("@diretor", filmes.Diretor),
-                    new MySqlParameter("@mcu", filmes.MCU),
-                    new MySqlParameter("@estrelas", filmes.Estrelas),
-                    new MySqlParameter("@estudio", filmes.Estudio)
+                int rowsAffected = _conn.InsertData("series",
+                    new MySqlParameter("@codigo", series.Codigo),
+                    new MySqlParameter("@titulo", series.Titulo),
+                    new MySqlParameter("@audio", series.Audio),
+                    new MySqlParameter("@sinopse", series.Sinopse),
+                    new MySqlParameter("@titulo_original", series.TituloOriginal),
+                    new MySqlParameter("@data_lancamento", series.DataLancamento),
+                    new MySqlParameter("@titulo_alternativo", series.TituloAlternativo),
+                    new MySqlParameter("@franquia", series.Franquia),
+                    new MySqlParameter("@genero", series.Genero),
+                    new MySqlParameter("@tags", series.Tags),
+                    new MySqlParameter("@diretor", series.Diretor),
+                    new MySqlParameter("@mcu", series.MCU),
+
+                    new MySqlParameter( "@estrelas", series.Estrelas),
+                    new MySqlParameter("@estudio", series.Estudio) ,
+                    new MySqlParameter(
+
+
                 );
 
                 if (rowsAffected > 0)
                 {
-                    MessageBox.Show(filmes.Titulo + " cadastrado com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(series.Titulo + " cadastrado com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
