d273aee5e4cd2de45c7eadecde3e66ce296200eb
Cybermacks <nnexxus15@proton.me>
2025-02-05 23:14:07 -0300

    Series.cs

--- Models/Series.cs
+++ Models/Series.cs
@@ -2,6 +2,7 @@
 {
     public class Series
     {
+        public string? Codigo { get; set; }
         public string? Titulo { get; set; }
         public string? Audio { get; set; }
         public string? Sinopse { get; set; }
