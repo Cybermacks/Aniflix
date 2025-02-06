83fd14d3925ff5f13c8857bd8b68a3738c1c5cf6
Cybermacks <nnexxus15@proton.me>
2025-02-05 22:02:49 -0300

    SeriesView.cs

--- Views/SeriesView.cs
+++ Views/SeriesView.cs
@@ -46,7 +46,7 @@ public partial class SeriesView : Form
                     DataLancamentoText.Text = given.FirstAirDate?.ToString("dd/MM/yyyy");
                     PaisOrigemText.Text = "#" + StringExtensions.RemoveDiacritics(country.Text.Replace(" ", ""));
                     IdiomaOriginalText.Text = "#" + StringExtensions.RemoveDiacritics(language.Text.Replace(" ", ""));
-                    SerieText.Text = "#" + StringExtensions.RemoveDiacritics(given.Name.Replace(" ", ""));
+                    SerieText.Text = "#" + StringExtensions.RemoveExcept(given.Name.Replace(" ", ""));
                 }));
 
                 if (
