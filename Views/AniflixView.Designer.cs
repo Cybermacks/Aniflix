1e871fb2f5d01427d26d3156533e16b8b038b821
Cybermacks <nnexxus15@proton.me>
2025-02-06 22:17:54 -0300

    AnyText.Designer.cs

--- Views/AniflixView.Designer.cs
+++ Views/AniflixView.Designer.cs
@@ -28,82 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
             roundedButton1 = new Custom.RoundedButton();
-            anyCombo1 = new Custom.AnyCombo();
-            anyText1 = new Custom.AnyText();
+            roundedButton2 = new Custom.RoundedButton();
+            roundedButton3 = new Custom.RoundedButton();
+            roundedButton4 = new Custom.RoundedButton();
+            roundedButton5 = new Custom.RoundedButton();
+            roundedButton6 = new Custom.RoundedButton();
+            roundedButton7 = new Custom.RoundedButton();
+            roundedButton8 = new Custom.RoundedButton();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(31, 30, 68);
+            panel1.Controls.Add(roundedButton8);
+            panel1.Controls.Add(roundedButton7);
+            panel1.Controls.Add(roundedButton6);
+            panel1.Controls.Add(roundedButton5);
+            panel1.Controls.Add(roundedButton4);
+            panel1.Controls.Add(roundedButton3);
+            panel1.Controls.Add(roundedButton2);
+            panel1.Controls.Add(roundedButton1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(280, 924);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(280, 100);
+            panel2.TabIndex = 0;
             // 
             // roundedButton1
             // 
-            roundedButton1.BackColor = Color.MediumSlateBlue;
-            roundedButton1.BackgroundColor = Color.MediumSlateBlue;
+            roundedButton1.BackColor = Color.Transparent;
+            roundedButton1.BackgroundColor = Color.Transparent;
             roundedButton1.BorderColor = Color.PaleVioletRed;
             roundedButton1.BorderRadius = 0;
             roundedButton1.BorderSize = 0;
+            roundedButton1.Dock = DockStyle.Top;
             roundedButton1.FlatAppearance.BorderSize = 0;
             roundedButton1.FlatStyle = FlatStyle.Flat;
             roundedButton1.ForeColor = Color.White;
-            roundedButton1.Location = new Point(197, 211);
+            roundedButton1.Location = new Point(0, 100);
             roundedButton1.Name = "roundedButton1";
-            roundedButton1.Size = new Size(150, 137);
+            roundedButton1.Size = new Size(280, 74);
             roundedButton1.TabIndex = 1;
-            roundedButton1.Text = "Filmes";
+            roundedButton1.Text = "roundedButton1";
             roundedButton1.TextColor = Color.White;
             roundedButton1.UseVisualStyleBackColor = false;
             // 
-            // anyCombo1
+            // roundedButton2
             // 
-            anyCombo1.BackColor = Color.WhiteSmoke;
-            anyCombo1.BorderColor = Color.MediumSlateBlue;
-            anyCombo1.BorderSize = 1;
-            anyCombo1.DropDownStyle = ComboBoxStyle.DropDown;
-            anyCombo1.Font = new Font("Segoe UI", 10F);
-            anyCombo1.ForeColor = Color.DimGray;
-            anyCombo1.IconColor = Color.MediumSlateBlue;
-            anyCombo1.ListBackColor = Color.FromArgb(230, 228, 245);
-            anyCombo1.ListTextColor = Color.DimGray;
-            anyCombo1.Location = new Point(561, 242);
-            anyCombo1.MinimumSize = new Size(200, 30);
-            anyCombo1.Name = "anyCombo1";
-            anyCombo1.Padding = new Padding(1);
-            anyCombo1.Size = new Size(200, 30);
-            anyCombo1.TabIndex = 2;
-            anyCombo1.Texts = "";
+            roundedButton2.BackColor = Color.Transparent;
+            roundedButton2.BackgroundColor = Color.Transparent;
+            roundedButton2.BorderColor = Color.PaleVioletRed;
+            roundedButton2.BorderRadius = 0;
+            roundedButton2.BorderSize = 0;
+            roundedButton2.Dock = DockStyle.Top;
+            roundedButton2.FlatAppearance.BorderSize = 0;
+            roundedButton2.FlatStyle = FlatStyle.Flat;
+            roundedButton2.ForeColor = Color.White;
+            roundedButton2.Location = new Point(0, 174);
+            roundedButton2.Name = "roundedButton2";
+            roundedButton2.Size = new Size(280, 74);
+            roundedButton2.TabIndex = 2;
+            roundedButton2.Text = "roundedButton2";
+            roundedButton2.TextColor = Color.White;
+            roundedButton2.UseVisualStyleBackColor = false;
             // 
-            // anyText1
+            // roundedButton3
             // 
-            anyText1.BorderColor = Color.MediumSlateBlue;
-            anyText1.BorderFocusColor = Color.HotPink;
-            anyText1.BorderRadius = 0;
-            anyText1.BorderSize = 2;
-            anyText1.Location = new Point(385, 371);
-            anyText1.Multiline = false;
-            anyText1.Name = "anyText1";
-            anyText1.PasswordChar = false;
-            anyText1.PlaceholderColor = Color.DarkGray;
-            anyText1.PlaceholderText = "";
-            anyText1.Size = new Size(526, 23);
-            anyText1.TabIndex = 3;
-            anyText1.Texts = "";
-            anyText1.UnderlinedStyle = false;
+            roundedButton3.BackColor = Color.Transparent;
+            roundedButton3.BackgroundColor = Color.Transparent;
+            roundedButton3.BorderColor = Color.PaleVioletRed;
+            roundedButton3.BorderRadius = 0;
+            roundedButton3.BorderSize = 0;
+            roundedButton3.Dock = DockStyle.Top;
+            roundedButton3.FlatAppearance.BorderSize = 0;
+            roundedButton3.FlatStyle = FlatStyle.Flat;
+            roundedButton3.ForeColor = Color.White;
+            roundedButton3.Location = new Point(0, 248);
+            roundedButton3.Name = "roundedButton3";
+            roundedButton3.Size = new Size(280, 74);
+            roundedButton3.TabIndex = 3;
+            roundedButton3.Text = "roundedButton3";
+            roundedButton3.TextColor = Color.White;
+            roundedButton3.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton4
+            // 
+            roundedButton4.BackColor = Color.Transparent;
+            roundedButton4.BackgroundColor = Color.Transparent;
+            roundedButton4.BorderColor = Color.PaleVioletRed;
+            roundedButton4.BorderRadius = 0;
+            roundedButton4.BorderSize = 0;
+            roundedButton4.Dock = DockStyle.Top;
+            roundedButton4.FlatAppearance.BorderSize = 0;
+            roundedButton4.FlatStyle = FlatStyle.Flat;
+            roundedButton4.ForeColor = Color.White;
+            roundedButton4.Location = new Point(0, 322);
+            roundedButton4.Name = "roundedButton4";
+            roundedButton4.Size = new Size(280, 74);
+            roundedButton4.TabIndex = 4;
+            roundedButton4.Text = "roundedButton4";
+            roundedButton4.TextColor = Color.White;
+            roundedButton4.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton5
+            // 
+            roundedButton5.BackColor = Color.Transparent;
+            roundedButton5.BackgroundColor = Color.Transparent;
+            roundedButton5.BorderColor = Color.PaleVioletRed;
+            roundedButton5.BorderRadius = 0;
+            roundedButton5.BorderSize = 0;
+            roundedButton5.Dock = DockStyle.Top;
+            roundedButton5.FlatAppearance.BorderSize = 0;
+            roundedButton5.FlatStyle = FlatStyle.Flat;
+            roundedButton5.ForeColor = Color.White;
+            roundedButton5.Location = new Point(0, 396);
+            roundedButton5.Name = "roundedButton5";
+            roundedButton5.Size = new Size(280, 74);
+            roundedButton5.TabIndex = 5;
+            roundedButton5.Text = "roundedButton5";
+            roundedButton5.TextColor = Color.White;
+            roundedButton5.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton6
+            // 
+            roundedButton6.BackColor = Color.Transparent;
+            roundedButton6.BackgroundColor = Color.Transparent;
+            roundedButton6.BorderColor = Color.PaleVioletRed;
+            roundedButton6.BorderRadius = 0;
+            roundedButton6.BorderSize = 0;
+            roundedButton6.Dock = DockStyle.Top;
+            roundedButton6.FlatAppearance.BorderSize = 0;
+            roundedButton6.FlatStyle = FlatStyle.Flat;
+            roundedButton6.ForeColor = Color.White;
+            roundedButton6.Location = new Point(0, 470);
+            roundedButton6.Name = "roundedButton6";
+            roundedButton6.Size = new Size(280, 74);
+            roundedButton6.TabIndex = 6;
+            roundedButton6.Text = "roundedButton6";
+            roundedButton6.TextColor = Color.White;
+            roundedButton6.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton7
+            // 
+            roundedButton7.BackColor = Color.Transparent;
+            roundedButton7.BackgroundColor = Color.Transparent;
+            roundedButton7.BorderColor = Color.PaleVioletRed;
+            roundedButton7.BorderRadius = 0;
+            roundedButton7.BorderSize = 0;
+            roundedButton7.Dock = DockStyle.Top;
+            roundedButton7.FlatAppearance.BorderSize = 0;
+            roundedButton7.FlatStyle = FlatStyle.Flat;
+            roundedButton7.ForeColor = Color.White;
+            roundedButton7.Location = new Point(0, 544);
+            roundedButton7.Name = "roundedButton7";
+            roundedButton7.Size = new Size(280, 74);
+            roundedButton7.TabIndex = 7;
+            roundedButton7.Text = "roundedButton7";
+            roundedButton7.TextColor = Color.White;
+            roundedButton7.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton8
+            // 
+            roundedButton8.BackColor = Color.Transparent;
+            roundedButton8.BackgroundColor = Color.Transparent;
+            roundedButton8.BorderColor = Color.PaleVioletRed;
+            roundedButton8.BorderRadius = 0;
+            roundedButton8.BorderSize = 0;
+            roundedButton8.Dock = DockStyle.Top;
+            roundedButton8.FlatAppearance.BorderSize = 0;
+            roundedButton8.FlatStyle = FlatStyle.Flat;
+            roundedButton8.ForeColor = Color.White;
+            roundedButton8.Location = new Point(0, 618);
+            roundedButton8.Name = "roundedButton8";
+            roundedButton8.Size = new Size(280, 74);
+            roundedButton8.TabIndex = 8;
+            roundedButton8.Text = "roundedButton8";
+            roundedButton8.TextColor = Color.White;
+            roundedButton8.UseVisualStyleBackColor = false;
             // 
             // AniflixView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1122, 613);
-            Controls.Add(anyText1);
-            Controls.Add(anyCombo1);
-            Controls.Add(roundedButton1);
+            ClientSize = new Size(1687, 924);
+            Controls.Add(panel1);
             Name = "AniflixView";
             Text = "AniflixView";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
+        private Panel panel1;
         private Custom.RoundedButton roundedButton1;
-        private Custom.AnyCombo anyCombo1;
-        private Custom.AnyText anyText1;
+        private Panel panel2;
+        private Custom.RoundedButton roundedButton8;
+        private Custom.RoundedButton roundedButton7;
+        private Custom.RoundedButton roundedButton6;
+        private Custom.RoundedButton roundedButton5;
+        private Custom.RoundedButton roundedButton4;
+        private Custom.RoundedButton roundedButton3;
+        private Custom.RoundedButton roundedButton2;
     }
 }
