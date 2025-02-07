ea9a9e21f4792672239a81dc64ebef653d28718d
Cybermacks <nnexxus15@proton.me>
2025-02-06 21:54:58 -0300

    AnyText.cs

--- Custom/AnyText.cs
+++ Custom/AnyText.cs
@@ -339,14 +339,12 @@ namespace Aniflix.Custom
             this.Invalidate();
             RemovePlaceholder();
         }
-        private void textBox1_Leave(object sender, EventArgs e)
+        private void TextBox1_Leave(object sender, EventArgs e)
         {
             isFocused = false;
             this.Invalidate();
             SetPlaceholder();
         }
         #endregion
-    }
-}
     }
 }
