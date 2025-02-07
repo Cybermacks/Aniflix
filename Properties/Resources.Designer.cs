543d69eb8647b2d9523ee0f821140cea9e7d0fa0
Cybermacks <nnexxus15@proton.me>
2025-02-07 09:47:11 -0300

    Resources.Designer.cs Resources.resx window-close.png

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Aniflix.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap window_minimize {
+            get {
+                object obj = ResourceManager.GetObject("window-minimize", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
