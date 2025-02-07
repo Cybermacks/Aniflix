cc8098cfd3aba3bd92804d97176f40acc7eda0b3
Cybermacks <nnexxus15@proton.me>
2025-02-06 22:26:07 -0300

    AniflixView.Designer.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Aniflix.Custom;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aniflix.Views
 {
     public partial class AniflixView : Form
     {
+        private RoundedButton? currentBtn;
+        private Panel? leftBorderBtn;
+
         public AniflixView()
         {
             InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panel1.Controls.Add(leftBorderBtn);
         }
     }
 }
