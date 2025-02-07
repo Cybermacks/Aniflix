f87aeea27a0772a8292a8904c88a43428da2f839
Cybermacks <nnexxus15@proton.me>
2025-02-06 22:40:06 -0300

    AniflixView.cs

--- Views/AniflixView.Designer.cs
+++ Views/AniflixView.Designer.cs
@@ -40,12 +40,14 @@
             panel2 = new Panel();
             panel3 = new Panel();
             panelDesktop = new Panel();
+            roundedButton1 = new Custom.RoundedButton();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(31, 30, 68);
+            panel1.Controls.Add(roundedButton1);
             panel1.Controls.Add(roundedButton8);
             panel1.Controls.Add(roundedButton7);
             panel1.Controls.Add(roundedButton6);
@@ -77,7 +79,7 @@
             roundedButton8.Padding = new Padding(10, 0, 20, 0);
             roundedButton8.Size = new Size(280, 74);
             roundedButton8.TabIndex = 8;
-            roundedButton8.Text = "roundedButton8";
+            roundedButton8.Text = "TrashFlix";
             roundedButton8.TextColor = Color.White;
             roundedButton8.UseVisualStyleBackColor = false;
             // 
@@ -97,7 +99,7 @@
             roundedButton7.Padding = new Padding(10, 0, 20, 0);
             roundedButton7.Size = new Size(280, 74);
             roundedButton7.TabIndex = 7;
-            roundedButton7.Text = "roundedButton7";
+            roundedButton7.Text = "Gore Séries";
             roundedButton7.TextColor = Color.White;
             roundedButton7.UseVisualStyleBackColor = false;
             // 
@@ -117,7 +119,7 @@
             roundedButton6.Padding = new Padding(10, 0, 20, 0);
             roundedButton6.Size = new Size(280, 74);
             roundedButton6.TabIndex = 6;
-            roundedButton6.Text = "roundedButton6";
+            roundedButton6.Text = "Gore Filmes";
             roundedButton6.TextColor = Color.White;
             roundedButton6.UseVisualStyleBackColor = false;
             // 
@@ -137,7 +139,7 @@
             roundedButton5.Padding = new Padding(10, 0, 20, 0);
             roundedButton5.Size = new Size(280, 74);
             roundedButton5.TabIndex = 5;
-            roundedButton5.Text = "roundedButton5";
+            roundedButton5.Text = "BreakOut Séries";
             roundedButton5.TextColor = Color.White;
             roundedButton5.UseVisualStyleBackColor = false;
             // 
@@ -157,7 +159,7 @@
             roundedButton4.Padding = new Padding(10, 0, 20, 0);
             roundedButton4.Size = new Size(280, 74);
             roundedButton4.TabIndex = 4;
-            roundedButton4.Text = "roundedButton4";
+            roundedButton4.Text = "BreakOut Filmes";
             roundedButton4.TextColor = Color.White;
             roundedButton4.UseVisualStyleBackColor = false;
             // 
@@ -177,7 +179,7 @@
             roundedButton3.Padding = new Padding(10, 0, 20, 0);
             roundedButton3.Size = new Size(280, 74);
             roundedButton3.TabIndex = 3;
-            roundedButton3.Text = "roundedButton3";
+            roundedButton3.Text = "Animes";
             roundedButton3.TextColor = Color.White;
             roundedButton3.UseVisualStyleBackColor = false;
             // 
@@ -197,7 +199,7 @@
             roundedButton2.Padding = new Padding(10, 0, 20, 0);
             roundedButton2.Size = new Size(280, 74);
             roundedButton2.TabIndex = 2;
-            roundedButton2.Text = "roundedButton2";
+            roundedButton2.Text = "Séries";
             roundedButton2.TextColor = Color.White;
             roundedButton2.UseVisualStyleBackColor = false;
             // 
@@ -246,6 +248,26 @@
             panelDesktop.Size = new Size(1407, 824);
             panelDesktop.TabIndex = 2;
             // 
+            // roundedButton1
+            // 
+            roundedButton1.BackColor = Color.Transparent;
+            roundedButton1.BackgroundColor = Color.Transparent;
+            roundedButton1.BorderColor = Color.PaleVioletRed;
+            roundedButton1.BorderRadius = 0;
+            roundedButton1.BorderSize = 0;
+            roundedButton1.Dock = DockStyle.Top;
+            roundedButton1.FlatAppearance.BorderSize = 0;
+            roundedButton1.FlatStyle = FlatStyle.Flat;
+            roundedButton1.ForeColor = Color.White;
+            roundedButton1.Location = new Point(0, 686);
+            roundedButton1.Name = "roundedButton1";
+            roundedButton1.Padding = new Padding(10, 0, 20, 0);
+            roundedButton1.Size = new Size(280, 74);
+            roundedButton1.TabIndex = 9;
+            roundedButton1.Text = "Fechar";
+            roundedButton1.TextColor = Color.White;
+            roundedButton1.UseVisualStyleBackColor = false;
+            // 
             // AniflixView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -275,5 +297,6 @@
         private Custom.RoundedButton roundedButton2;
         private Panel panel3;
         private Panel panelDesktop;
+        private Custom.RoundedButton roundedButton1;
     }
 }
