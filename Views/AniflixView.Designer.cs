8e7e2a9408be1ed9ffcee67bd771458f29e4d3f8
Cybermacks <nnexxus15@proton.me>
2025-02-08 14:02:23 -0300

    AnyText.cs

--- Views/AniflixView.Designer.cs
+++ Views/AniflixView.Designer.cs
@@ -28,18 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AniflixView));
             MenuPanel = new Panel();
-            FecharButton = new Custom.RoundedButton();
-            TrashFlixButton = new Custom.RoundedButton();
-            GoreSeriesButton = new Custom.RoundedButton();
-            GoreFilmesButton = new Custom.RoundedButton();
-            BreakOutSeriesButton = new Custom.RoundedButton();
-            BreakOutFilmesButton = new Custom.RoundedButton();
-            AnimesFilmesButton = new Custom.RoundedButton();
-            AnimesButton = new Custom.RoundedButton();
-            SeriesButton = new Custom.RoundedButton();
-            FilmesButton = new Custom.RoundedButton();
             panel2 = new Panel();
             HomeButton = new PictureBox();
             TitleBarPanel = new Panel();
@@ -48,6 +37,8 @@
             iconCurrentChildForm = new PictureBox();
             lblTitleChildForm = new Label();
             panelDesktop = new Panel();
+            colorDialog1 = new ColorDialog();
+            button1 = new Button();
             MenuPanel.SuspendLayout();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)HomeButton).BeginInit();
@@ -60,229 +51,13 @@
             // MenuPanel
             // 
             MenuPanel.BackColor = Color.FromArgb(31, 30, 68);
-            MenuPanel.Controls.Add(FecharButton);
-            MenuPanel.Controls.Add(TrashFlixButton);
-            MenuPanel.Controls.Add(GoreSeriesButton);
-            MenuPanel.Controls.Add(GoreFilmesButton);
-            MenuPanel.Controls.Add(BreakOutSeriesButton);
-            MenuPanel.Controls.Add(BreakOutFilmesButton);
-            MenuPanel.Controls.Add(AnimesFilmesButton);
-            MenuPanel.Controls.Add(AnimesButton);
-            MenuPanel.Controls.Add(SeriesButton);
-            MenuPanel.Controls.Add(FilmesButton);
+            MenuPanel.Controls.Add(button1);
             MenuPanel.Controls.Add(panel2);
             MenuPanel.Dock = DockStyle.Left;
             MenuPanel.Location = new Point(0, 0);
             MenuPanel.Name = "MenuPanel";
             MenuPanel.Size = new Size(280, 924);
             MenuPanel.TabIndex = 0;
-            // 
-            // FecharButton
-            // 
-            FecharButton.BackColor = Color.Transparent;
-            FecharButton.BackgroundColor = Color.Transparent;
-            FecharButton.BorderColor = Color.PaleVioletRed;
-            FecharButton.BorderRadius = 0;
-            FecharButton.BorderSize = 0;
-            FecharButton.Dock = DockStyle.Top;
-            FecharButton.FlatAppearance.BorderSize = 0;
-            FecharButton.FlatStyle = FlatStyle.Flat;
-            FecharButton.ForeColor = Color.White;
-            FecharButton.Location = new Point(0, 640);
-            FecharButton.Name = "FecharButton";
-            FecharButton.Padding = new Padding(10, 0, 20, 0);
-            FecharButton.Size = new Size(280, 60);
-            FecharButton.TabIndex = 10;
-            FecharButton.Text = "Fechar";
-            FecharButton.TextColor = Color.White;
-            FecharButton.UseVisualStyleBackColor = false;
-            FecharButton.Click += FecharButton_Click;
-            // 
-            // TrashFlixButton
-            // 
-            TrashFlixButton.BackColor = Color.Transparent;
-            TrashFlixButton.BackgroundColor = Color.Transparent;
-            TrashFlixButton.BorderColor = Color.PaleVioletRed;
-            TrashFlixButton.BorderRadius = 0;
-            TrashFlixButton.BorderSize = 0;
-            TrashFlixButton.Dock = DockStyle.Top;
-            TrashFlixButton.FlatAppearance.BorderSize = 0;
-            TrashFlixButton.FlatStyle = FlatStyle.Flat;
-            TrashFlixButton.ForeColor = Color.White;
-            TrashFlixButton.Location = new Point(0, 580);
-            TrashFlixButton.Name = "TrashFlixButton";
-            TrashFlixButton.Padding = new Padding(10, 0, 20, 0);
-            TrashFlixButton.Size = new Size(280, 60);
-            TrashFlixButton.TabIndex = 9;
-            TrashFlixButton.Text = "TrashFlix";
-            TrashFlixButton.TextColor = Color.White;
-            TrashFlixButton.UseVisualStyleBackColor = false;
-            // 
-            // GoreSeriesButton
-            // 
-            GoreSeriesButton.BackColor = Color.Transparent;
-            GoreSeriesButton.BackgroundColor = Color.Transparent;
-            GoreSeriesButton.BorderColor = Color.PaleVioletRed;
-            GoreSeriesButton.BorderRadius = 0;
-            GoreSeriesButton.BorderSize = 0;
-            GoreSeriesButton.Dock = DockStyle.Top;
-            GoreSeriesButton.FlatAppearance.BorderSize = 0;
-            GoreSeriesButton.FlatStyle = FlatStyle.Flat;
-            GoreSeriesButton.ForeColor = Color.White;
-            GoreSeriesButton.Location = new Point(0, 520);
-            GoreSeriesButton.Name = "GoreSeriesButton";
-            GoreSeriesButton.Padding = new Padding(10, 0, 20, 0);
-            GoreSeriesButton.Size = new Size(280, 60);
-            GoreSeriesButton.TabIndex = 8;
-            GoreSeriesButton.Text = "Gore Séries";
-            GoreSeriesButton.TextColor = Color.White;
-            GoreSeriesButton.UseVisualStyleBackColor = false;
-            // 
-            // GoreFilmesButton
-            // 
-            GoreFilmesButton.BackColor = Color.Transparent;
-            GoreFilmesButton.BackgroundColor = Color.Transparent;
-            GoreFilmesButton.BorderColor = Color.PaleVioletRed;
-            GoreFilmesButton.BorderRadius = 0;
-            GoreFilmesButton.BorderSize = 0;
-            GoreFilmesButton.Dock = DockStyle.Top;
-            GoreFilmesButton.FlatAppearance.BorderSize = 0;
-            GoreFilmesButton.FlatStyle = FlatStyle.Flat;
-            GoreFilmesButton.ForeColor = Color.White;
-            GoreFilmesButton.Location = new Point(0, 460);
-            GoreFilmesButton.Name = "GoreFilmesButton";
-            GoreFilmesButton.Padding = new Padding(10, 0, 20, 0);
-            GoreFilmesButton.Size = new Size(280, 60);
-            GoreFilmesButton.TabIndex = 7;
-            GoreFilmesButton.Text = "Gore Filmes";
-            GoreFilmesButton.TextColor = Color.White;
-            GoreFilmesButton.UseVisualStyleBackColor = false;
-            // 
-            // BreakOutSeriesButton
-            // 
-            BreakOutSeriesButton.BackColor = Color.Transparent;
-            BreakOutSeriesButton.BackgroundColor = Color.Transparent;
-            BreakOutSeriesButton.BorderColor = Color.PaleVioletRed;
-            BreakOutSeriesButton.BorderRadius = 0;
-            BreakOutSeriesButton.BorderSize = 0;
-            BreakOutSeriesButton.Dock = DockStyle.Top;
-            BreakOutSeriesButton.FlatAppearance.BorderSize = 0;
-            BreakOutSeriesButton.FlatStyle = FlatStyle.Flat;
-            BreakOutSeriesButton.ForeColor = Color.White;
-            BreakOutSeriesButton.Location = new Point(0, 400);
-            BreakOutSeriesButton.Name = "BreakOutSeriesButton";
-            BreakOutSeriesButton.Padding = new Padding(10, 0, 20, 0);
-            BreakOutSeriesButton.Size = new Size(280, 60);
-            BreakOutSeriesButton.TabIndex = 6;
-            BreakOutSeriesButton.Text = "BreakOut Séries";
-            BreakOutSeriesButton.TextColor = Color.White;
-            BreakOutSeriesButton.UseVisualStyleBackColor = false;
-            // 
-            // BreakOutFilmesButton
-            // 
-            BreakOutFilmesButton.BackColor = Color.Transparent;
-            BreakOutFilmesButton.BackgroundColor = Color.Transparent;
-            BreakOutFilmesButton.BorderColor = Color.PaleVioletRed;
-            BreakOutFilmesButton.BorderRadius = 0;
-            BreakOutFilmesButton.BorderSize = 0;
-            BreakOutFilmesButton.Dock = DockStyle.Top;
-            BreakOutFilmesButton.FlatAppearance.BorderSize = 0;
-            BreakOutFilmesButton.FlatStyle = FlatStyle.Flat;
-            BreakOutFilmesButton.ForeColor = Color.White;
-            BreakOutFilmesButton.Location = new Point(0, 340);
-            BreakOutFilmesButton.Name = "BreakOutFilmesButton";
-            BreakOutFilmesButton.Padding = new Padding(10, 0, 20, 0);
-            BreakOutFilmesButton.Size = new Size(280, 60);
-            BreakOutFilmesButton.TabIndex = 5;
-            BreakOutFilmesButton.Text = "BreakOut Filmes";
-            BreakOutFilmesButton.TextColor = Color.White;
-            BreakOutFilmesButton.UseVisualStyleBackColor = false;
-            // 
-            // AnimesFilmesButton
-            // 
-            AnimesFilmesButton.BackColor = Color.Transparent;
-            AnimesFilmesButton.BackgroundColor = Color.Transparent;
-            AnimesFilmesButton.BorderColor = Color.PaleVioletRed;
-            AnimesFilmesButton.BorderRadius = 0;
-            AnimesFilmesButton.BorderSize = 0;
-            AnimesFilmesButton.Dock = DockStyle.Top;
-            AnimesFilmesButton.FlatAppearance.BorderSize = 0;
-            AnimesFilmesButton.FlatStyle = FlatStyle.Flat;
-            AnimesFilmesButton.ForeColor = Color.White;
-            AnimesFilmesButton.Location = new Point(0, 280);
-            AnimesFilmesButton.Name = "AnimesFilmesButton";
-            AnimesFilmesButton.Padding = new Padding(10, 0, 20, 0);
-            AnimesFilmesButton.Size = new Size(280, 60);
-            AnimesFilmesButton.TabIndex = 4;
-            AnimesFilmesButton.Text = "Animes Filmes";
-            AnimesFilmesButton.TextColor = Color.White;
-            AnimesFilmesButton.UseVisualStyleBackColor = false;
-            // 
-            // AnimesButton
-            // 
-            AnimesButton.BackColor = Color.Transparent;
-            AnimesButton.BackgroundColor = Color.Transparent;
-            AnimesButton.BorderColor = Color.PaleVioletRed;
-            AnimesButton.BorderRadius = 0;
-            AnimesButton.BorderSize = 0;
-            AnimesButton.Dock = DockStyle.Top;
-            AnimesButton.FlatAppearance.BorderSize = 0;
-            AnimesButton.FlatStyle = FlatStyle.Flat;
-            AnimesButton.ForeColor = Color.White;
-            AnimesButton.Location = new Point(0, 220);
-            AnimesButton.Name = "AnimesButton";
-            AnimesButton.Padding = new Padding(10, 0, 20, 0);
-            AnimesButton.Size = new Size(280, 60);
-            AnimesButton.TabIndex = 3;
-            AnimesButton.Text = "Animes";
-            AnimesButton.TextColor = Color.White;
-            AnimesButton.UseVisualStyleBackColor = false;
-            AnimesButton.Click += AnimesButton_Click;
-            // 
-            // SeriesButton
-            // 
-            SeriesButton.BackColor = Color.Transparent;
-            SeriesButton.BackgroundColor = Color.Transparent;
-            SeriesButton.BorderColor = Color.PaleVioletRed;
-            SeriesButton.BorderRadius = 0;
-            SeriesButton.BorderSize = 0;
-            SeriesButton.Dock = DockStyle.Top;
-            SeriesButton.FlatAppearance.BorderSize = 0;
-            SeriesButton.FlatStyle = FlatStyle.Flat;
-            SeriesButton.ForeColor = Color.White;
-            SeriesButton.Location = new Point(0, 160);
-            SeriesButton.Name = "SeriesButton";
-            SeriesButton.Padding = new Padding(10, 0, 20, 0);
-            SeriesButton.Size = new Size(280, 60);
-            SeriesButton.TabIndex = 2;
-            SeriesButton.Text = "Séries";
-            SeriesButton.TextColor = Color.White;
-            SeriesButton.UseVisualStyleBackColor = false;
-            SeriesButton.Click += SeriesButton_Click;
-            // 
-            // FilmesButton
-            // 
-            FilmesButton.BackColor = Color.Transparent;
-            FilmesButton.BackgroundColor = Color.Transparent;
-            FilmesButton.BackgroundImageLayout = ImageLayout.Stretch;
-            FilmesButton.BorderColor = Color.PaleVioletRed;
-            FilmesButton.BorderRadius = 0;
-            FilmesButton.BorderSize = 0;
-            FilmesButton.Dock = DockStyle.Top;
-            FilmesButton.FlatAppearance.BorderSize = 0;
-            FilmesButton.FlatStyle = FlatStyle.Flat;
-            FilmesButton.ForeColor = Color.White;
-            FilmesButton.Image = (Image)resources.GetObject("FilmesButton.Image");
-            FilmesButton.ImageAlign = ContentAlignment.MiddleLeft;
-            FilmesButton.Location = new Point(0, 100);
-            FilmesButton.Name = "FilmesButton";
-            FilmesButton.Padding = new Padding(10, 0, 20, 0);
-            FilmesButton.Size = new Size(280, 60);
-            FilmesButton.TabIndex = 1;
-            FilmesButton.Text = "Filmes";
-            FilmesButton.TextColor = Color.White;
-            FilmesButton.UseVisualStyleBackColor = false;
-            FilmesButton.Click += Filmesutton_Click;
             // 
             // panel2
             // 
@@ -366,6 +141,17 @@
             panelDesktop.Size = new Size(1407, 844);
             panelDesktop.TabIndex = 2;
             // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Location = new Point(0, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(280, 49);
+            button1.TabIndex = 1;
+            button1.Text = "Filmes";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // AniflixView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -390,23 +176,16 @@
         #endregion
 
         private Panel MenuPanel;
-        private Custom.RoundedButton FilmesButton;
         private Panel panel2;
-        private Custom.RoundedButton SeriesButton;
         private Panel TitleBarPanel;
         private Panel panelDesktop;
         private Label lblTitleChildForm;
         private PictureBox iconCurrentChildForm;
         private PictureBox MinimizeButton;
         private PictureBox CloseButton;
-        private Custom.RoundedButton FecharButton;
-        private Custom.RoundedButton TrashFlixButton;
-        private Custom.RoundedButton GoreSeriesButton;
-        private Custom.RoundedButton GoreFilmesButton;
-        private Custom.RoundedButton BreakOutSeriesButton;
-        private Custom.RoundedButton BreakOutFilmesButton;
-        private Custom.RoundedButton AnimesFilmesButton;
-        private Custom.RoundedButton AnimesButton;
+
         private PictureBox HomeButton;
+        private ColorDialog colorDialog1;
+        private Button button1;
     }
 }
