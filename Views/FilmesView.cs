9751b8cccb1211a9e18e776768bcdcaaf5f33e47
Cybermacks <nnexxus15@proton.me>
2025-02-09 14:27:22 -0300

    SeriesView.cs

--- Views/FilmesView.cs
+++ Views/FilmesView.cs
@@ -28,7 +28,6 @@ namespace Aniflix.Views
             try
             {
 
-
                 var general = GlobFunctions.MovieDatabase();
 
                 var movieTask = general.GetMovieAsync(CodigoText.Text);
