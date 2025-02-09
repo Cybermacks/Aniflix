d3b83e71cbea7ce12586016daaa0dad76cc913e5
Cybermacks <nnexxus15@proton.me>
2025-02-09 12:32:27 -0300

    GoreFilmes.Designer.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Aniflix
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AniflixView());
+            Application.Run(new GoreFilmes());
         }
     }
 }
--- Views/GoreFilmes.cs
+++ Views/GoreFilmes.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Aniflix.Views
+﻿namespace Aniflix.Views
 {
     public partial class GoreFilmes : Form
     {
         public GoreFilmes()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
