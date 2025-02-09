eeb55fffbff4ae3b049ec4d10d85372b7f0fc7d0
Cybermacks <nnexxus15@proton.me>
2025-02-09 14:59:33 -0300

    FilmesView.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Aniflix
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new GoreFilmes());
+            Application.Run(new AniflixView());
         }
     }
 }
