429cf105ef3a14a5a653a0470294591c53fe17a2
Cybermacks <nnexxus15@proton.me>
2025-02-09 12:24:47 -0300

    SeriesView.cs

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -8,6 +8,6 @@ namespace Aniflix.Globals
         public static int currentId = 0;
         public static bool editando = false;
         public static string TMDB_KEY = "1dcbf681735d3e7454953f5b7c22b6dc";
-        public static string DEEPL_KEY = "";
+        public static string DEEPL_KEY = "7feb3eb8-de95-4312-843c-1064aecdab8b:fx";
     }
 }
