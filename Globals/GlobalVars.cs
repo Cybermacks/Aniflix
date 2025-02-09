96e0fdb86f9eb403984b98f9bd78c5fffb9f9fc4
Cybermacks <nnexxus15@proton.me>
2025-02-09 11:54:11 -0300

    GlobalVars.cs

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -4,33 +4,9 @@ namespace Aniflix.Globals
 {
     public static class GlobalVars
     {
-        private static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
-        private static int _currentId = 0;
-        private static bool _editando = false;
-        private static string _api_key = "1dcbf681735d3e7454953f5b7c22b6dc";
-
-
-        public static string API_KEY
-        {
-
-            get { return API_KEY; }
-            private set { API_KEY = value; }
-        }
-
-        public static string ApiKey => _api_key;
-
-        public static bool Editando
-        {
-            get { return _editando; }
-            set { _editando = value; }
-        }
-
-        public static int CurrentId
-        {
-            get { return _currentId; }
-            set { _currentId = value; }
-        }
-
-
+        public static readonly DatabaseConnection _conn = DatabaseConnection.Instance;
+        public static int currentId = 0;
+        public static bool editando = false;
+        public static string TMDB_KEY = "1dcbf681735d3e7454953f5b7c22b6dc";
     }
 }
