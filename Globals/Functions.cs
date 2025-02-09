4466477275b9223e0752b0b6d56916b2136e4f84
Cybermacks <nnexxus15@proton.me>
2025-02-09 11:46:19 -0300

    Functions.cs

--- Globals/Functions.cs
+++ Globals/Functions.cs
@@ -62,7 +62,8 @@ namespace Aniflix.Globals
         private extern static void SendMessage(nint hWnd, int wMsg, int wParam, int lParam);
 
 
-        [DllImport("dwmapi.dll")]
+        //[DllImport("dwmapi.dll")]
+        [LibraryImport("dwmapi.dll")]
         private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
 
         [DllImport("uxtheme.dll", EntryPoint = "#135", SetLastError = true)]
