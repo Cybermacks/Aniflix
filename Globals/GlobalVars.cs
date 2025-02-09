bd0eca19da446cc6d2f342d03651790ed1197df1
Cybermacks <nnexxus15@proton.me>
2025-02-09 11:50:30 -0300

    Functions.cs

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -8,5 +8,11 @@ namespace Aniflix.Globals
         public static int currentId = 0;
         public static bool editando = false;
         public static string API_KEY = "1dcbf681735d3e7454953f5b7c22b6dc";
+
+        public static string API_KEY
+        {
+            get { return API_KEY; }
+            set { API_KEY = value; }
+        }
     }
 }
